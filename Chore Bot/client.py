6c551618ce0517cad8aae330edcb925887a6434b
Keshyyyy <keshmuthu1543@gmail.com>
2025-10-14 14:01:48 -0700

    update client

--- Chore Bot/client.py
+++ Chore Bot/client.py
@@ -387,7 +387,7 @@ async def overdue(interaction: discord.Interaction):
     
         if len(overdue) > 10:
             embed.set_footer(
-                icon_url=interaction.author.display_avatar.url,
+                icon_url=getattr(interaction.user.display_avatar, 'url', None),
                 text= f'Showing 10 of {len(overdue)} items past due, completed or not'
             )
     
